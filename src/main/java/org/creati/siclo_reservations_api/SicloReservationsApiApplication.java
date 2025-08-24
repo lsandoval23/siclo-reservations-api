b24a2c03b76222fbc59d9dc7821c9197973d2be8
Luis Sandoval <leancco@stefanini.com>
2025-08-23 21:28:10 -0500

    first commit

---
+++ src/main/java/org/creati/siclo_reservations_api/SicloReservationsApiApplication.java
@@ -0,0 +1,13 @@
+package org.creati.siclo_reservations_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SicloReservationsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SicloReservationsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/creati/siclo_reservations_api/SicloReservationsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.creati.siclo_reservations_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SicloReservationsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
