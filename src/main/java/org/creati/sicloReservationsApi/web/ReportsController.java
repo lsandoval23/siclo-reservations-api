5cb83b4801222dc2dc4372a7f308b9e9dea3e244
Luis Sandoval <lsandovalancco@gmail.com>
2025-10-29 23:35:37 -0500

    Code cleanup

--- src/main/java/org/creati/sicloReservationsApi/web/ReportsController.java
+++ src/main/java/org/creati/sicloReservationsApi/web/ReportsController.java
@@ -5,6 +5,7 @@ import org.creati.sicloReservationsApi.service.ReportService;
 import org.creati.sicloReservationsApi.service.model.reports.ClientReservationsPaymentsDto;
 import org.creati.sicloReservationsApi.service.model.reports.PagedResponse;
 import org.creati.sicloReservationsApi.service.model.reports.PaymentTableDto;
+import org.creati.sicloReservationsApi.service.model.reports.ReservationGraphReportDto;
 import org.creati.sicloReservationsApi.service.model.reports.ReservationTableDto;
 import org.creati.sicloReservationsApi.service.model.reports.SortDirection;
 import org.springframework.format.annotation.DateTimeFormat;
@@ -23,16 +24,16 @@ import java.util.List;
 @Validated
 @RestController
 @PreAuthorize("hasAuthority('REPORT_VIEW')")
-@RequestMapping("/reports/clients")
-public class ClientReportsController {
+@RequestMapping("/reports")
+public class ReportsController {
 
     private final ReportService reportService;
 
-    public ClientReportsController(final ReportService reportService) {
+    public ReportsController(final ReportService reportService) {
         this.reportService = reportService;
     }
 
-    @GetMapping("")
+    @GetMapping("/clients")
     public List<ClientReservationsPaymentsDto> getReservationsPaymentsReport(
             @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate from,
             @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate to,
@@ -45,7 +46,7 @@ public class ClientReportsController {
         return reportService.getClientReservationsPayments(from, to, clientId);
     }
 
-    @GetMapping("/{clientId}/reservations")
+    @GetMapping("/clients/{clientId}/reservations")
     public ResponseEntity<PagedResponse<ReservationTableDto>> getReservationTableByClient(
             @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate from,
             @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate to,
@@ -70,7 +71,7 @@ public class ClientReportsController {
 
     }
 
-    @GetMapping("/{clientId}/payments")
+    @GetMapping("/clients/{clientId}/payments")
     public ResponseEntity<PagedResponse<PaymentTableDto>> getPaymentTableByClient(
             @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate from,
             @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate to,
@@ -95,6 +96,63 @@ public class ClientReportsController {
 
     }
 
+    @GetMapping("/reservations")
+    public ReservationGraphReportDto getReservationGraphReport(
+            @RequestParam String groupBy,
+            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate from,
+            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate to,
+            @RequestParam(defaultValue = "day") String timeUnit
+    ) {
+        if (!from.isBefore(to)) {
+            throw new IllegalArgumentException("From date must be before To date");
+        }
+        return reportService.getReservationGroupedReport(ReservationGraphReportDto.GroupBy.fromValue(groupBy), from, to, timeUnit);
+    }
 
 
+    @GetMapping("/reservations/table")
+    public ResponseEntity<PagedResponse<ReservationTableDto>> getReservationTable(
+            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate from,
+            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate to,
+            @Min(value = 0, message = "Page number must be 0 or greater")
+            @RequestParam(defaultValue = "0") int page,
+            @Min(value = 1, message = "Page size must be at least 1")
+            @RequestParam(defaultValue = "10") int size,
+            @RequestParam String sortBy,
+            @RequestParam(defaultValue = "asc") String sortDir
+    ) {
+        if (!from.isBefore(to)) {
+            throw new IllegalArgumentException("From date must be before To date");
+        }
+
+        return ResponseEntity.ok().body(
+                reportService.getReservationTable(
+                        from, to, page, size,
+                        ReservationTableDto.ReservationSortField.fromValue(sortBy),
+                        SortDirection.fromValue(sortDir)));
+    }
+
+    @GetMapping("/payments/table")
+    public ResponseEntity<PagedResponse<PaymentTableDto>> getPaymentTable(
+            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate from,
+            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate to,
+            @Min(value = 0, message = "Page number must be 0 or greater")
+            @RequestParam(defaultValue = "0") int page,
+            @Min(value = 1, message = "Page size must be at least 1")
+            @RequestParam(defaultValue = "10") int size,
+            @RequestParam String sortBy,
+            @RequestParam(defaultValue = "asc") String sortDir
+    ) {
+        if (!from.isBefore(to)) {
+            throw new IllegalArgumentException("From date must be before To date");
+        }
+
+        return ResponseEntity.ok().body(
+                reportService.getPaymentTable(
+                        from, to, page, size,
+                        PaymentTableDto.PaymentSortFiled.fromValue(sortBy),
+                        SortDirection.fromValue(sortDir)
+                )
+        );
+    }
 }
